a159e2a84c991d929e7fe4fdeeb7e342e6ca6489
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-04 18:32:43 -0400

    Update play.js

--- play.js
+++ play.js
@@ -2,4 +2,35 @@ const net = require('net');
 const connect = require('../d3/client');
 console.log('Connecting ...');
 connect();
-
+let connection;
+const handleUserInput = function (input, conn) {
+  if (input === '\u0003') {
+    process.exit();
+  } else if (input === 'w') {
+    conn.write("Move: up");
+  } else if (input === 's') {
+    conn.write('Move: down');
+  } else if (input === 'a') {
+    conn.write('Move: left');
+  } else if (input === 'd') {
+    conn.write('Move: right');
+  } else if (input === 'm') {
+    conn.write('Say: HEYYY');
+  }
+};
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', data => {
+    handleUserInput(data, conn);
+  })
+  return stdin;
+}; 
+module.exports = setupInput();
