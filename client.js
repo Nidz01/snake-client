019698e3974a1b849be74deb6623461bec462fc4
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 19:43:22 -0400

    client.js
    To connect with server

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+/*
+ Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  /* An event handler to handle incoming 
+  data and console log it for the player.
+  */
+  conn.on('data', (data) => {
+    console.log('you ded cuz you idled'); // a message sent to us from the server when it kicks out our snake for idling...
+  });
+
+  return conn;
+}
+
+module.exports = connect;
