da32e79be119bea62c12f0b5f87c0575bc413fd6
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 19:33:55 -0400

    Update play.js

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
-const connect = require('../d3/client');
-const setUpInput = require('../d3/input');
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  /* An event handler to handle incoming 
+  data and console log it for the player.
+  */
+  conn.on('data', (data) => {
+    console.log('you ded cuz you idled'); // a message sent to us from the server when it kicks out our snake for idling...
+  });
+
+  return conn;
+}
+
 console.log('Connecting ...');
 connect();
-setupInput();
