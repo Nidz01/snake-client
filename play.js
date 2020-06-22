c3c8dfc198dda1246e13359e9603c907dc838aea
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 19:40:12 -0400

    Update play.js
    Importing function connect from other file client.js

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  /* An event handler to handle incoming 
-  data and console log it for the player.
-  */
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled'); // a message sent to us from the server when it kicks out our snake for idling...
-  });
-
-  return conn;
-}
+// Importing function connect from other file client.js
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
