9fa3203984abca4cf040815b9869add982fa4092
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 22:49:24 -0400

    Update client.js
    "data" callback handler for stdin and terminate the game

--- client.js
+++ client.js
@@ -43,4 +43,26 @@ const connect = function() {
   return conn;
 };
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput = function(key) {
+    if(key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on('data', handleUserInput); //callback to pass any key strokes to hUI;
+  return stdin;
+};
+
+
+setupInput();
+
 module.exports = connect;
