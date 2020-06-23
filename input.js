13498e3c8de74d87478325afbf96728d36be0a3b
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 23:18:52 -0400

    Update input.js
    with all direction handler

--- input.js
+++ input.js
@@ -1,9 +1,12 @@
+// Stores the active TCP connection object.
+let connection;
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -13,9 +16,13 @@ const setupInput = function() {
     if(key === '\u0003') {
       process.exit();
     }
+    if(key === 'w') {conn.write('Move: up')};
+    if(key === 'a') {conn.write('Move: up')};
+    if(key === 's') {conn.write('Move: left')};
+    if(key === 'd') {conn.write('Move: right')};
   };
   stdin.on('data', handleUserInput); //callback to pass any key strokes to hUI;
   return stdin;
-};
+}
 
-module.exports = {setUpInput};
+module.exports = setupInput;
