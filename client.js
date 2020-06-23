dde9ee3df2510c9ece51cc8b18727e8930bd910a
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 23:26:13 -0400

    Update client.js
    move any CONSTANT_VALUES into a separate module

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 const net = require('net');
+const { IP, PORT } = require('./constant');
 /*
  Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    IP, PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
